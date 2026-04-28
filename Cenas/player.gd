9316edc21178c8dafa7958c56e3773772852ac1a
MacGyverClash02 <venomjep@gmail.com>
2026-04-28 14:08:54 -0300

    Mods++FaseJp
    - Adiciona detecção de colisão em paredes e entre inimigos (Goomba e Koopa).
    - Implementa física do casco do Koopa (rebate no cenário e elimina inimigos).
    - Cria bloco surpresa animado e sistema de spawn de itens.
    - Adiciona física, gravidade e movimentação autônoma ao cogumelo.
    - Atualiza o Player: colisão de teto para blocos, mecânica de crescer com ajuste dinâmico de hitbox e sistema de dano para perda de power-up.

--- Cenas/player.gd
+++ Cenas/player.gd
@@ -1,5 +1,4 @@
 extends CharacterBody2D
-
 class_name Player
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -33,9 +32,12 @@ const POINTS_LABEL_SCENE = preload("res://Cenas/points_label.tscn")
 
 var player_mode = PlayerMode.SMALL
 var is_dead = false
+var is_transforming = false
 
 func _physics_process(delta):
-	
+	if is_transforming:
+		return
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
@@ -48,6 +50,13 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= 0.5
+
+	if is_on_ceiling():
+		for i in get_slide_collision_count():
+			var colisao = get_slide_collision(i)
+			var collider = colisao.get_collider()
+			if collider and collider.has_method("bater_por_baixo"):
+				collider.bater_por_baixo()
 		
 	var direction = Input.get_axis("left", "right")
 	
@@ -57,7 +66,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed * delta)
 		
 	animated_sprite_2d.trigger_animation(velocity, direction, player_mode)
-
 	move_and_slide()
 
 func _on_area_2d_area_entered(area):
@@ -68,12 +76,20 @@ func handle_enemy_collision(enemy: Enemy):
 	if enemy == null || is_dead:
 		return
 	
+	var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
+	var is_stomping = angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision
+	
 	if is_instance_of(enemy, Koopa) and (enemy as Koopa).in_a_shell:
-		(enemy as Koopa).on_stomp(global_position)
+		if (enemy as Koopa).horizontal_speed == 0:
+			(enemy as Koopa).on_stomp(global_position)
+		else:
+			if is_stomping:
+				(enemy as Koopa).horizontal_speed = 0
+				on_enemy_stomped()
+			else:
+				die()
 	else:
-		var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
-		
-		if angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision:
+		if is_stomping:
 			enemy.die()
 			on_enemy_stomped()
 			spawn_points_label(enemy)
@@ -99,3 +115,42 @@ func die():
 		death_tween.tween_property(self, "position", position + Vector2(0, -48), .5)
 		death_tween.chain().tween_property(self, "position", position + Vector2(0, 256), 1)
 		death_tween.tween_callback(func (): GameManager.lose_life())
+	else:
+		player_mode = PlayerMode.SMALL
+		
+		var nova_forma_corpo = RectangleShape2D.new()
+		nova_forma_corpo.size = Vector2(16, 16)
+		body_collision_shape_2d.shape = nova_forma_corpo
+		body_collision_shape_2d.position.y = 0
+		
+		var nova_forma_area = RectangleShape2D.new()
+		nova_forma_area.size = Vector2(16, 16)
+		area_collision_shape_2d.shape = nova_forma_area
+		area_collision_shape_2d.position.y = 0
+		
+		animated_sprite_2d.position.y = 0
+		
+		area_2d.set_deferred("monitoring", false)
+		await get_tree().create_timer(1.0).timeout
+		area_2d.set_deferred("monitoring", true)
+
+func crescer():
+	if player_mode == PlayerMode.SMALL:
+		is_transforming = true
+		animated_sprite_2d.play("small_to_big")
+		
+		var nova_forma_corpo = RectangleShape2D.new()
+		nova_forma_corpo.size = Vector2(16, 32)
+		body_collision_shape_2d.shape = nova_forma_corpo
+		body_collision_shape_2d.position.y = -8
+		
+		var nova_forma_area = RectangleShape2D.new()
+		nova_forma_area.size = Vector2(16, 32)
+		area_collision_shape_2d.shape = nova_forma_area
+		area_collision_shape_2d.position.y = -8
+		
+		animated_sprite_2d.position.y = -8
+		
+		await animated_sprite_2d.animation_finished
+		player_mode = PlayerMode.BIG
+		is_transforming = false
