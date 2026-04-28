4f5d18afc536535bab47a72e8bee9a51b0290ae7
Henrique <henriquelca@hotmail.com>
2026-04-27 21:51:08 -0300

    Merge parte da laila

--- Cenas/star.gd
+++ Cenas/star.gd
@@ -9,8 +9,8 @@ func collect():
 	var current_scene = get_tree().current_scene.scene_file_path
 	
 	if current_scene.contains("phase_1"):
-		get_tree().change_scene_to_file("res://Cenas/phase_2.tscn")
-	elif current_scene.contains("phase_2"):
+		get_tree().change_scene_to_file("res://Cenas/lailadc.tscn")
+	elif current_scene.contains("lailadc"):
 		get_tree().change_scene_to_file("res://Cenas/game_won.tscn")
 	
 	queue_free()
